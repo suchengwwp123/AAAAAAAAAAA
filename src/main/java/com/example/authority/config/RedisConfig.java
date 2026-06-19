ce6458f86de1aae1d34333d6853665e62a3e55b2
suchengwwp123 <suchengwwp123@github.com>
2026-06-20 02:52:54 +0800

    fix: exclude RedisConfig from cloud profile, use Spring Boot auto-config

--- src/main/java/com/example/authority/config/RedisConfig.java
+++ src/main/java/com/example/authority/config/RedisConfig.java
@@ -10,6 +10,7 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -23,6 +24,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
  * @author:authority
  */
 @Configuration
+@Profile("!cloud")
 public class RedisConfig {
 
     @Value("${spring.data.redis.host}")
